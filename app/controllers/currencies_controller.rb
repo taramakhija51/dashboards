b40d81c054317b7922be1b0d6b82ddcd725938ee
taramakhija51 <tmakhija@uchicago.edu>
2022-10-25 15:53:26 +0000

    complete!

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -16,4 +16,17 @@ class CurrenciesController < ApplicationController
     @from_symbol = params.fetch("from_currency")
     render({:template => "currency_templates/step_two.html.erb"})
   end
+
+  def third_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+    @rates_data = open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}").read
+    @parsed_rates = JSON.parse(@rates_data)
+    @rates = @parsed_rates.fetch("info").fetch("rate")
+    render({:template => "currency_templates/step_three.html.erb"})
+  end
 end
